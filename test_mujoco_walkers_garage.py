ab9a90589019bfb4a1c96eba05bd12d56b385017
hangyin <navigator8972@gmail.com>
2020-12-11 14:48:43 +0100

    Update to most recent garage. Tried vanilla PPO on halfcheetah, it unlearns -_-! I doubt this means Koopman is superior just because it learns, need to dig a bit more. Also, might need to consider wrap RL algo with the model learning held-out. Maybe implementation of MAML or other optimization-in-loop algo can be referenced...

--- test_mujoco_walkers_garage.py
+++ test_mujoco_walkers_garage.py
@@ -3,7 +3,10 @@ import gym
 
 from garage import wrap_experiment
 from garage.envs import GymEnv
+
 from garage.experiment.deterministic import set_seed
+from garage.sampler import RaySampler
+
 from garage.torch.algos import PPO
 from garage.torch.policies import GaussianMLPPolicy
 from garage.torch.value_functions import GaussianMLPValueFunction
@@ -27,25 +30,37 @@ def ppo_half_cheetah(ctxt=None, seed=1):
 
     trainer = Trainer(ctxt)
 
-    policy = GaussianKoopmanLQRPolicy(
-        env_spec=env.spec,
-        k=4,
-        T=5,
-        phi='FCNN',
-        residual=None,
-        # normal_distribution_cls=Normal,
-        init_std=1.0,
-    )
+    policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[64, 64],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+
+    # policy = GaussianKoopmanLQRPolicy(
+    #     env_spec=env.spec,
+    #     k=4,
+    #     T=5,
+    #     phi='FCNN',
+    #     residual=None,
+    #     # normal_distribution_cls=Normal,
+    #     init_std=1.0,
+    # )
 
     value_function = GaussianMLPValueFunction(env_spec=env.spec,
                                               hidden_sizes=(32, 32),
                                               hidden_nonlinearity=torch.tanh,
                                               output_nonlinearity=None)
 
+    sampler = RaySampler(agents=policy,
+                        envs=env,
+                        max_episode_length=env.spec.max_episode_length)
+
     algo = PPO(env_spec=env.spec,
                policy=policy,
                value_function=value_function,
+               sampler=sampler,
                discount=0.99,
+               gae_lambda=0.95,
+               lr_clip_range=0.2,
                center_adv=False)
 
     trainer.setup(algo, env)
