d4b60a8040c1241e6a3a9e6163cf169b9b8e875b
hangyin <navigator8972@gmail.com>
2020-12-16 20:19:58 +0100

    Add SAC baseline learns after a few iterations. Looks much more promising than PPO on halfcheetah. Also using a larger network for value func and policy seems to use up memory for visulize mujoco... Maybe consider to prioritize the implementation for off policy SAC.

--- test_mujoco_walkers_garage.py
+++ test_mujoco_walkers_garage.py
@@ -1,15 +1,17 @@
+import numpy as np
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 import gym
 
 from garage import wrap_experiment
-from garage.envs import GymEnv
+from garage.envs import GymEnv, normalize
 
 from garage.experiment.deterministic import set_seed
 from garage.sampler import RaySampler
 
 from garage.torch.algos import PPO
-from garage.torch.policies import GaussianMLPPolicy
+from garage.torch.policies import GaussianMLPPolicy, TanhGaussianMLPPolicy
 from garage.torch.value_functions import GaussianMLPValueFunction
 from garage.trainer import Trainer
 
@@ -27,39 +29,39 @@ def ppo_mujoco_walkers(ctxt=None, seed=1):
 
     """
     set_seed(seed)
-    env = GymEnv(gym.make('HalfCheetah-v2'))
+    env = normalize(GymEnv('HalfCheetah-v2'))
 
     trainer = Trainer(ctxt)
 
-    # policy = GaussianMLPPolicy(env.spec,
-    #                            hidden_sizes=[64, 64],
-    #                            hidden_nonlinearity=torch.tanh,
-    #                            output_nonlinearity=None)
+    policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[32, 32],
+                               hidden_nonlinearity=F.relu,
+                               output_nonlinearity=None)
 
-    in_dim = env.spec.observation_space.flat_dim
-    hidden_dim = 32
-    out_dim = env.spec.action_space.flat_dim
+    # in_dim = env.spec.observation_space.flat_dim
+    # hidden_dim = 32
+    # out_dim = env.spec.action_space.flat_dim
 
-    residual = nn.Sequential(
-        nn.Linear(in_dim, hidden_dim),
-        nn.GELU(),
-        nn.Linear(hidden_dim, hidden_dim),
-        nn.GELU(),
-        nn.Linear(hidden_dim, out_dim),
-    )
-    policy = GaussianKoopmanLQRPolicy(
-        env_spec=env.spec,
-        k=4,
-        T=5,
-        phi='FCNN',
-        residual=residual,
-        # normal_distribution_cls=Normal,
-        init_std=1.0,
-    )
+    # residual = nn.Sequential(
+    #     nn.Linear(in_dim, hidden_dim),
+    #     nn.GELU(),
+    #     nn.Linear(hidden_dim, hidden_dim),
+    #     nn.GELU(),
+    #     nn.Linear(hidden_dim, out_dim),
+    # )
+    # policy = GaussianKoopmanLQRPolicy(
+    #     env_spec=env.spec,
+    #     k=4,
+    #     T=5,
+    #     phi='FCNN',
+    #     residual=residual,
+    #     # normal_distribution_cls=Normal,
+    #     init_std=1.0,
+    # )
 
     value_function = GaussianMLPValueFunction(env_spec=env.spec,
                                               hidden_sizes=(32, 32),
-                                              hidden_nonlinearity=torch.tanh,
+                                              hidden_nonlinearity=F.relu,
                                               output_nonlinearity=None)
 
     sampler = RaySampler(agents=policy,
@@ -73,10 +75,72 @@ def ppo_mujoco_walkers(ctxt=None, seed=1):
                discount=0.99,
                gae_lambda=0.95,
                lr_clip_range=0.2,
-               center_adv=False)
+               center_adv=True)
 
     trainer.setup(algo, env)
     trainer.train(n_epochs=100, batch_size=1500, plot=True)
+    return
 
+from garage.replay_buffer import PathBuffer
+from garage.sampler import FragmentWorker, LocalSampler
+from garage.torch import set_gpu_mode
+from garage.torch.algos import SAC
 
-ppo_mujoco_walkers(seed=1)
+from garage.torch.q_functions import ContinuousMLPQFunction
+
+@wrap_experiment(snapshot_mode='none')
+def sac_mujoco_walkers(ctxt=None, seed=1):
+    set_seed(seed)
+    trainer = Trainer(snapshot_config=ctxt)
+    env = normalize(GymEnv('HalfCheetah-v2'))
+
+    policy = TanhGaussianMLPPolicy(
+        env_spec=env.spec,
+        hidden_sizes=[256, 256],
+        hidden_nonlinearity=nn.ReLU,
+        output_nonlinearity=None,
+        min_std=np.exp(-20.),
+        max_std=np.exp(2.),
+    )
+
+    qf1 = ContinuousMLPQFunction(env_spec=env.spec,
+                                 hidden_sizes=[256, 256],
+                                 hidden_nonlinearity=F.relu)
+
+    qf2 = ContinuousMLPQFunction(env_spec=env.spec,
+                                 hidden_sizes=[256, 256],
+                                 hidden_nonlinearity=F.relu)
+
+    replay_buffer = PathBuffer(capacity_in_transitions=int(1e6))
+
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           worker_class=FragmentWorker)
+
+    sac = SAC(env_spec=env.spec,
+              policy=policy,
+              qf1=qf1,
+              qf2=qf2,
+              sampler=sampler,
+              gradient_steps_per_itr=1000,
+              max_episode_length_eval=1000,
+              replay_buffer=replay_buffer,
+              min_buffer_size=1e4,
+              target_update_tau=5e-3,
+              discount=0.99,
+              buffer_batch_size=256,
+              reward_scale=1.,
+              steps_per_epoch=1)
+
+    if torch.cuda.is_available():
+        set_gpu_mode(True)
+    else:
+        set_gpu_mode(False)
+    sac.to()
+    trainer.setup(algo=sac, env=env)
+    trainer.train(n_epochs=1000, batch_size=1000, plot=True)
+    return
+
+ppo_mujoco_walkers(seed=521)
+# sac_mujoco_walkers(seed=521)
