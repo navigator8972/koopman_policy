b7c4a839e2501b0a1ba6128b603d1bb9feb4d4b7
hangyin <navigator8972@gmail.com>
2021-05-10 16:45:35 +0200

    make visualize_garage_policy support softgym

--- visualize_garage_policy.py
+++ visualize_garage_policy.py
@@ -37,7 +37,8 @@ def rollout(env,
             animated=False,
             video_name=None,
             pause_per_frame=None,
-            deterministic=False):
+            deterministic=False,
+            is_softgym=False):
     """Sample a single episode of the agent in the environment.
     Args:
         agent (Policy): Policy used to select actions.
@@ -73,50 +74,80 @@ def rollout(env,
     agent_infos = []
     observations = []
     if animated:
-        env.visualize()
-        #force headless bullet environment like inverted pendulum to start the server with GUI
-        if hasattr(env._env, 'isRender'):
-            env._env.isRender = True 
+        if not is_softgym:
+            env.visualize()
+            #force headless bullet environment like inverted pendulum to start the server with GUI
+            if hasattr(env._env, 'isRender'):
+                env._env.isRender = True 
 
     last_obs, episode_infos = env.reset()
     agent.reset()
     episode_length = 0
+
+    img_size_softgym = 720
     
     image_array = []    
     while episode_length < (max_episode_length or np.inf):
-        img = env._env.render(mode='rgb_array')
-        image_array.append(img)
+        if not is_softgym:
+            img = env._env.render(mode='rgb_array')
+            image_array.append(img)
 
         if pause_per_frame is not None:
             time.sleep(pause_per_frame)
         a, agent_info = agent.get_action(last_obs)
         if deterministic and 'mean' in agent_info:
             a = agent_info['mean']
-        es = env.step(a)
+        if not is_softgym:
+            es = env.step(a)
+        else:
+            # print(env, env._env._env)
+            obs, reward, done, info = env._env._env.step(a, record_continuous_video=True, img_size=img_size_softgym)
+            image_array.extend(info['flex_env_recorded_frames'])
+            es = {'action':a, 'reward':reward, 'env_info':info, 'observation':obs, 'terminal':done}
+
         env_steps.append(es)
         observations.append(last_obs)
         agent_infos.append(agent_info)
         episode_length += 1
-        if es.last:
-            break
-        last_obs = es.observation
+    
+        if not is_softgym:
+            if es.last:
+                break
+            last_obs = es.observation
+        else:
+            if es['terminal']:       #is done
+                break
+            last_obs = es['observation']
+        
     
     if video_name is not None:
         save_numpy_to_video_matplotlib(image_array, video_name, interval=pause_per_frame*1000)
 
-    return dict(
-        episode_infos=episode_infos,
-        observations=np.array(observations),
-        actions=np.array([es.action for es in env_steps]),
-        rewards=np.array([es.reward for es in env_steps]),
-        agent_infos=stack_tensor_dict_list(agent_infos),
-        env_infos=stack_tensor_dict_list([es.env_info for es in env_steps]),
-        dones=np.array([es.terminal for es in env_steps]),
-    )
+    if not is_softgym:
+        return dict(
+            episode_infos=episode_infos,
+            observations=np.array(observations),
+            actions=np.array([es.action for es in env_steps]),
+            rewards=np.array([es.reward for es in env_steps]),
+            agent_infos=stack_tensor_dict_list(agent_infos),
+            env_infos=stack_tensor_dict_list([es.env_info for es in env_steps]),
+            dones=np.array([es.terminal for es in env_steps]),
+        )
+    else:
+        return dict(
+            episode_infos=episode_infos,
+            observations=np.array(observations),
+            actions=np.array([es['action'] for es in env_steps]),
+            rewards=np.array([es['reward'] for es in env_steps]),
+            agent_infos=stack_tensor_dict_list(agent_infos),
+            env_infos=stack_tensor_dict_list([es['env_info'] for es in env_steps]),
+            dones=np.array([es['terminal'] for es in env_steps]),
+        )
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--snapshot_dir', type=str, default='')
+    parser.add_argument('--use_softgym', type=int, default=0)
 
     args = parser.parse_args()
 
@@ -132,10 +163,20 @@ def main():
     env = data['env']
 
     policy.cpu()
-    
+
+    if args.use_softgym:
+        try:
+            import pyflex
+            #headless, render, width, height
+            pyflex.init(0, True, env._env.camera_width, env._env.camera_height) 
+            env.reset()
+        except ImportError as e:
+            raise error.DependencyNotInstalled("{}. (You need to first compile the python binding)".format(e))
+            return
+
     # See what the trained policy can accomplish
     print('Starting to run a rollout...')
-    path = rollout(env, policy, max_episode_length=500, animated=True, video_name='test', pause_per_frame=0.01, deterministic=True)
+    path = rollout(env, policy, max_episode_length=500, animated=True, video_name='test', pause_per_frame=0.01, deterministic=True, is_softgym=args.use_softgym)
     
     return
 
